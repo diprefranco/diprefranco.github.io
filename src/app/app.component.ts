b9d289880bfb4f791fbe90f880f5b990e6e2a819
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-10-15 16:12:48 -0300

    Hamburger menu: configuration behaviour added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //footer
   readonly currentYear = new Date().getFullYear();
+
+  ngOnInit() {
+    //header
+    this.setHamburgerMenuConfig();
+  }
+
+  setHamburgerMenuConfig() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    if (hamburger && navMenu) {
+      this.addHamburgerClickEvent(hamburger, navMenu);
+      this.addNavLinksClickEvent(hamburger, navMenu);
+    }
+  }
+
+  private addHamburgerClickEvent(hamburger: Element, navMenu: Element) {
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+  }
+
+  private addNavLinksClickEvent(hamburger: Element, navMenu: Element) {
+    document.querySelectorAll(".nav-link").forEach((n) =>
+      n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+      })
+    );
+  }
 }
