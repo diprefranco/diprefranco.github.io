bc2d4996efc7295870c5baa68235b1030ab02aa2
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-10-19 21:46:37 -0300

    Experience section added.

---
+++ src/app/layout/experience/experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.css'
+})
+export class ExperienceComponent {
+}
