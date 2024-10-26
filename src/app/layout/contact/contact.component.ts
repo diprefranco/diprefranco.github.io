b4caca5c574b16f031071ea5e9d8afa982c0f076
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-10-26 11:08:35 -0300

    Contact section added.

---
+++ src/app/layout/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+}
