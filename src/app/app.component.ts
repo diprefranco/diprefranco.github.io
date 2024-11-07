c10b45e9fd84ef5e4b645db145a271867676a34a
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-11-06 23:16:40 -0300

    Console welcome message added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from "./layout/footer/footer.component";
 import { HomeComponent } from './pages/home/home.component';
@@ -14,5 +14,32 @@ import { HomeComponent } from './pages/home/home.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  readonly currentYear = new Date().getFullYear();
+
+  ngOnInit() {
+    this.logConsoleWelcomeMessage();
+  }
+
+  private logConsoleWelcomeMessage() {
+    setTimeout(() => {
+      console.log(`%c
+                                                                                                                         
+  ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ███████╗███████╗██╗     ██╗      █████╗ ███████╗██╗  
+  ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ██╔════╝██╔════╝██║     ██║     ██╔══██╗██╔════╝██║  
+  ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      █████╗  █████╗  ██║     ██║     ███████║███████╗██║  
+  ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      ██╔══╝  ██╔══╝  ██║     ██║     ██╔══██║╚════██║╚═╝  
+  ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    ██║     ███████╗███████╗███████╗██║  ██║███████║██╗  
+   ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚═╝     ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  
+                                                                                                                         
+  ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗  
+  ║                      The site code you're exploring might be minimized.                                           ║  
+  ║                      Check it out here: https://github.com/diprefranco/diprefranco.github.io                      ║  
+  ║                                                                                                                   ║  
+  ║                                                © ${this.currentYear} Franco Dipre                                                ║  
+  ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝  
+                                                                                                                         `,
+        "font-size: 14px; color: #0abefb; background-color: #191919");
+    }, 1000);
+  }
 }
