831b669cd71a44625de69c1d9ebc0d15d272186a
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-13 14:58:29 -0300

    404 page: add animation when page loads.

--- src/app/pages/home/not-found/not-found.component.ts
+++ src/app/pages/home/not-found/not-found.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { ImgComponent } from '../../../shared/img/img.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'web-not-found',
   standalone: true,
-  imports: [ImgComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+
+  ngOnInit() {
+    this.setAnimation();
+  }
+
+  private setAnimation() {
+    setTimeout(() => {
+      document.querySelector('.animate')?.classList.add('visible');
+    }, 0.1);
+  }
 }
